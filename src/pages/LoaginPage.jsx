bc24bbcbcb65a0ee91ea28ed6329117515261d99
Darshana-ctrls <darshanapotdar2001@gmail.com>
2025-09-09 21:10:54 +0530

    Login Page Completed

--- src/pages/LoaginPage.jsx
+++ src/pages/LoaginPage.jsx
@@ -2,7 +2,21 @@ import { useState } from 'react'
 import assets from '../assets/assets'
 const LoaginPage = () => {
 
-  const [currentState,setCurrentState] =useState("Sign up")
+  const [currentState, setCurrentState] = useState("Sign up")
+  const [fullName, setFullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [bio, setBio] = useState("")
+  const [isDataSubmitted, setisDataSubmitted] = useState(false)
+
+  const onSubmitHandler=(event)=>{
+    event.preventDefault();
+    if(currentState === "Sign up" && !isDataSubmitted ){
+      setisDataSubmitted(true)
+      return;
+    }
+  }
+
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
@@ -10,12 +24,60 @@ const LoaginPage = () => {
       <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
 
       {/* right */}
-      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6
+      <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6
 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex justify-between items-center'>
           {currentState}
-          <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
-          </h2>
+          {isDataSubmitted && <img src={assets.arrow_icon} onClick={()=>setisDataSubmitted(false)} 
+          className='w-5 cursor-pointer' />}
+          
+        </h2>
+        {currentState === "Sign up" && !isDataSubmitted && (
+          <input onChange={(e) => setFullName(e.target.value)} value={fullName}
+            type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required />
+        )}
+
+        {!isDataSubmitted && (
+          <>
+            <input onChange={(e) => setEmail(e.target.value)} value={email}
+              type="email" required placeholder='Email Address'
+              className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' />
+            <input onChange={(e) => setPassword(e.target.value)} value={password}
+              type="password" required placeholder='Password'
+              className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' />
+          </>
+        )}
+
+        {
+          currentState  === "Sign up" && isDataSubmitted &&(
+            <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+            rows={4} placeholder='Provide a short bio' required
+            className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'></textarea>
+          )
+        }
+
+        <button type='submit'
+        className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
+          {currentState === "Sign up" ? "Create Account" : "Login Now"}
+        </button>
+
+        <div className='flex items-center gap-2 text-sm text-gray-500'>
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+          {currentState === "Sign up" ?(
+            <p className='text-sm text-gray-600'>Already have an account? 
+            <span onClick={()=>{setCurrentState("Login"); setisDataSubmitted(false)}} 
+             className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+          ):(
+            <p className='text-sm text-gray-600'>Create an account 
+            <span onClick={()=>{setCurrentState("Sign up")}} 
+            className='font-medium text-violet-500 cursor-pointer'>Click Here</span></p>
+          )}
+        </div>
+
       </form>
 
     </div>
