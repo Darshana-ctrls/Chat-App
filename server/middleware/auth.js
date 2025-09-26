ece51f6eb69732f85b2fc6a6e2708e3c97d02b93
Darshana-ctrls <darshanapotdar2001@gmail.com>
2025-09-26 10:48:47 +0530

    controller completed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,18 @@
 // it contains the function which will execute before the execution of controller function
 // protecting routes i.e. only if user is authenticated then can access api endpoint
 import User from "../models/user.js"
+import jwt from "jsonwebtoken"
 export const protectRoute = async (req,res,next)=>{
     try {
         const token = req.headers.token;
-        const user = await User.findById()
+        const decoded = jwt.verify(token.process.env.JWT_SECRET)
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user) return res.json({success:false,message:"User not found"});
+        req.user = user
+        next();
     } catch (error) {
-        
+        console.log(error.message)
+        res.json({success:false,message:error.message});
     }
 }
