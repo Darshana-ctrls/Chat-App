fca5f7e086fe3e6c0fa8a7d23bb38fdc39389b72
Darshana-ctrls <darshanapotdar2001@gmail.com>
2025-10-04 11:57:11 +0530

    message controller started

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,7 @@
 // getall users except the logged in user
 import Message from "../models/message.js"
-import User from "../models/user";
+import User from "../models/user.js";
+import cloudinary  from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async(req,res)=>{
     try {
@@ -10,10 +11,87 @@ export const getUsersForSidebar = async(req,res)=>{
         // count no. of messages not seen
         const unseenmessages = {}
         const promises = filteredUsers.map(async (user)=> {
-            
+            // get unseen messages
+            const messages = await Message.find({senderId:user._id,receiverId:userId,seen:false})
+            if(messages.length >0){
+                unseenmessages[user._id] = messages.length;
+            }
         })
+        await Promise.all(promises);
+        res.json({success:true,users:filteredUsers,unseenmessages})
 
     } catch (error) {
-        
+        console.log(error.messages);
+        res.json({success:false,message:error.message})
+
+    }
+}
+
+// get all messages for selected users
+
+export const getMessages=async(req,res)=>{
+    try {
+
+        // get selected user id
+        const {id:selectUserId} = req.params;
+        // get my id
+        const myId = req.user._id;
+
+
+        const messages = await Message.find({
+            $or: [
+                {
+                    senderId:myId , receiverId:selectUserId
+                },
+                {
+                    senderId:selectUserId , receiverId:myId
+                },
+            ]
+        })
+
+        await Message.updateMany({senderId:selectUserId,receiverId:myId},{seen:true})
+        res.json({success:true,messages})
+
+    } catch (error) {
+        console.log(error.messages);
+        res.json({success:false,message:error.message})
+    }
+}
+
+// function to mark message as seen using message id
+export const markMessageAsSeen = async (req,res) =>{
+    try {
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id , {seen:true})
+        res.json({success:true})
+    } catch (error) {
+        console.log(error.messages);
+        res.json({success:false,message:error.message})
+    }
+}
+
+// send message to selected user
+
+export const sendMessage = async (req,res) => {
+    try {
+        const {text,image}=req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadresponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadresponse.secure_url;
+        }
+
+        const newMessage = await Message.create({
+            senderId,receiverId,text,image:imageUrl
+        })
+
+        res.json({success:true,newMessage});
+
+    } catch (error) {
+      console.log(error.messages);
+        res.json({success:false,message:error.message})   
     }
 }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 // instance of express app and http server
 // we r using http server cause soket.io supports ttp server
@@ -22,6 +23,7 @@ app.use(cors());
 app.use("/api/status", (req,res)=>res.send("Server is live"));
 // routes setup
 app.use("/api/auth",userRouter)
+app.use("/api/messages",messageRouter)
 
 // connect to mongodb
 await connectDB();
