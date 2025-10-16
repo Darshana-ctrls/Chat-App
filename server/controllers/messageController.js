61d034a0c477222542356a172857e00b8d8a5d24
Darshana-ctrls <darshanapotdar2001@gmail.com>
2025-10-16 10:04:46 +0530

    Backend Completed

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,6 +2,7 @@
 import Message from "../models/message.js"
 import User from "../models/user.js";
 import cloudinary  from "../lib/cloudinary.js";
+import {io,userSocketMap} from "../server.js"
 
 export const getUsersForSidebar = async(req,res)=>{
     try {
@@ -87,6 +88,12 @@ export const sendMessage = async (req,res) => {
         const newMessage = await Message.create({
             senderId,receiverId,text,image:imageUrl
         })
+
+        // Emit the new message to the receivers socket
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage)
+        }
 
         res.json({success:true,newMessage});
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import http from "http";
+import http, { Server } from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
@@ -10,6 +10,30 @@ import messageRouter from "./routes/messageRoutes.js";
 // we r using http server cause soket.io supports ttp server
 const app = express();
 const server = http.createServer(app);
+
+// initialize socket.io server
+export const io = new Server(server,{
+    cors:{origin:"*"}
+})
+
+// store online users
+export const userSocketMap = {}; //{userId:socketId}
+
+// socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // emit online users to all connected clients
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    socket.on("disconnect",()=>{
+        console.log("User disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    })
+})
 
 // middleware setup
 
